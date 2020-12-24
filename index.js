a97f0ab74166480be0e681c012bf23d0cd79b6d0
Ben Halverson <benhalverson33@gmail.com>
2020-12-24 12:21:13 -0800

    refactor: 💡 using spread operator to copy args object

--- index.js
+++ index.js
@@ -166,9 +166,7 @@ const resolvers = {
 
 			const user = {
 				id: uuidv4(),
-				name: args.name,
-				email: args.email,
-				age: args.age
+				...args
 			};
 
 			users.push(user);
@@ -185,10 +183,7 @@ const resolvers = {
 
 			const post = {
 				id: uuidv4(),
-				title: args.title,
-				body: args.body,
-				published: args.published,
-				author: args.author
+				...args
 			};
 
 			posts.push(post);
@@ -205,9 +200,7 @@ const resolvers = {
 
 			const comment = {
 				id: uuidv4(),
-				text: args.text,
-				author: args.author,
-				post: args.post
+				...args
 			};
 
 			comments.push(comment);
