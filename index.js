b17e1932a37d29757e38ae224b8aba95427083f8
Ben Halverson <benhalverson33@gmail.com>
2020-12-24 03:52:13 -0800

    feat: 🎸 Mutation setup

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import { GraphQLServer } from 'graphql-yoga';
+import { v4 as uuidv4 } from 'uuid';
 const users = [
 	{
 		id: '1',
@@ -79,6 +80,10 @@ const typeDefs = `
         post: Post!
     }
 
+		type Mutation {
+			createUser(name: String!, email: String!, age: Int): User!
+		}
+
     type User {
         id: ID!
         name: String!
@@ -145,6 +150,27 @@ const resolvers = {
 				body: '',
 				published: false
 			};
+		}
+	},
+	Mutation: {
+		createUser(parent, args, ctx, info) {
+			const emailTaken = users.some((user) => {
+				return user.email === args.email;
+			});
+
+			if (emailTaken) {
+				throw new Error('Email already taken');
+			}
+
+			const user = {
+				id: uuidv4(),
+				name: args.name,
+				email: args.email,
+				age: args.age,
+			}
+
+			users.push(user);
+			return user;
 		}
 	},
 	Post: {
