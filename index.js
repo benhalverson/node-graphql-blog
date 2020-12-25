cb814de52eb6b658975029a87050762842d66726
Ben Halverson <benhalverson33@gmail.com>
2020-12-24 17:02:20 -0800

    demo pubsub with graphql

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { GraphQLServer } from 'graphql-yoga';
+import { GraphQLServer, PubSub } from 'graphql-yoga';
 
 import db from './db.js';
 import Query from './resolvers/Query.js';
@@ -6,18 +6,23 @@ import Mutation from './resolvers/Mutation.js';
 import Post from './resolvers/Post.js';
 import Comment from './resolvers/Comment.js';
 import User from './resolvers/User.js';
+import Subscription from './resolvers/Subscription.js';
+
+const pubsub = new PubSub();
 
 const server = new GraphQLServer({
 	typeDefs: './schema.graphql',
 	resolvers: {
 		Query,
 		Mutation,
+		Subscription,
 		Post,
 		Comment,
 		User
 	},
 	context: {
-		db
+		db,
+		pubsub
 	}
 });
 
